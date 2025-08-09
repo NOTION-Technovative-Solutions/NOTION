664f0a2f27ea38011176c4776894a3b48ef235e7
Napster230503 <alamsyahfahmi99@gmail.comgit config user.email alamsyahfahmi99@gmail.com>
2025-08-09 17:56:55 +0700

    update

--- dashboard/Views/dashboard.php
+++ dashboard/Views/dashboard.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    <!-- <link rel="preload" href="./adminlte.css" as="style" /> -->
+    <title>NOTION</title>
 </head>
 <body>
     <ul class="navbar-nav">
